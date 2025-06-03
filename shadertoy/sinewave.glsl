614fe7007a498096d6fd84035a6d3499bed33c35
penkovski <l_penkovski@yahoo.com>
2025-06-03 09:25:34 +0300

    Sinewave with variating color and draw abscissa

--- shadertoy/sinewave.glsl
+++ shadertoy/sinewave.glsl
@@ -6,19 +6,21 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     // sine wave params
     float amplitude = 0.1;
     float frequency = 25.0;
-    float speed = 2.0;
 
-    float y = amplitude * sin(frequency * st.x + iTime * speed) + 0.5;
+    float y = amplitude * sin(frequency * st.x + iTime) + 0.5;
 
-    float waveThickness = 2.0 / iResolution.y;
+    float waveThickness = 3.0 / iResolution.y;
 
     float distanceToWave = abs(st.y - y);
 
     vec3 color = vec3(0.0);
 
     if (distanceToWave <= waveThickness) {
+        color = vec3(cos(iTime), 0.8, 0.3);
+    } else if (st.y > 0.495 && st.y < 0.5) {
         color = vec3(1.0);
     }
+
 
     fragColor = vec4(color, 1.0);
 }
